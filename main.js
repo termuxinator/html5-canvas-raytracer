6b830159cc18edb378d220c612ad813adb3dfe67
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-10 17:48:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -37,12 +37,12 @@ function main () {
 
   let objects = [
     createSphere([1.5,2.5,0.0],0.5,createMaterial([1.0,1.0,1.0],0.1,0.5,50.0,0.8)),
-    createSphere([0.0,2.5,-2.0],0.5,createMaterial([0.5,0.5,0.5],0.8,0.1,50.0,0.8)),
+    createSphere([0.0,2.5,-2.0],0.5,createMaterial([0.5,0.5,0.5],0.8,0.1,500.0,0.8)),
     createSphere([-1.5,2.5,0.0],0.5,createMaterial([1.0,1.0,1.0],0.8,0.5,50.0,0.2)),
     createSphere([-1.5,1.0,0.0],1.0,createMaterial([1.0,0.0,0.0],1.0,0.3,50.0,0.1)),
     createSphere([1.5,1.0,0.0],1.0,createMaterial([0.0,1.0,0.0],1.0,0.3,50.0,0.1)),
     createSphere([0.0,1.0,-2.0],1.0,createMaterial([0.0,0.0,1.0],1.0,0.3,50.0,0.1)),
-    createSphere([ 0.0,0.5,2.0],0.5,createMaterial([1.0,1.0,1.0],1.0,0.3,5.0,0.0)),
+    createSphere([ 0.0,0.5,2.0],0.5,createMaterial([1.0,1.0,1.0],1.0,0.2,5.0,0.0)),
     createSphere([0.0,-5000.0,0.0],5000,createMaterial([1.0,1.0,1.0],1.0,0.5,50.0,0.4)),
   //createSphere([0.0,0.0,0.0],5000,createMaterial([0.4,0.6,0.8],0.0,0.0,0.0,0.0))
   ];
