0a032208b80d1982d6c2d5a143e9dccc2a10fb61
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-10 13:36:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
 })();
 
 function main () {
-  let build = '242';
+  let build = '243';
   let canvas = document.getElementById('canvasID');
   canvas.width = document.body.clientWidth;
   canvas.height = document.body.clientHeight;
@@ -38,7 +38,7 @@ function main () {
 //let projD = canvas.height / (2*Math.tan(projA/2)); // vertical FOV
 
   let objects = [
-    createSphere([0.0,2.5,-2.0],0.5,createMaterial([1.0,1.0,1.0],0.9,1.0,1000.0,0.9)),
+    createSphere([0.0,2.5,-2.0],0.5,createMaterial([0.5,0.5,0.5],0.9,1.0,1000.0,0.9)),
     createSphere([-1.5,1.0,0.0],1.0,createMaterial([1.0,0.0,0.0],1.0,0.5,50.0,0.1)),
     createSphere([1.5,1.0,0.0],1.0,createMaterial([0.0,1.0,0.0],1.0,0.5,50.0,0.1)),
     createSphere([0.0,1.0,-2.0],1.0,createMaterial([0.0,0.0,1.0],1.0,0.5,50.0,0.1)),
@@ -81,12 +81,12 @@ function main () {
     }
     context.putImageData(colorbuf,0,0,0,0,canvas.width,canvas.height);
     let elapsed = Date.now() - timestamp;
-    let str = 'build #' + build + ' (' + elapsed + 'ms)';
+    let message = 'build #' + build + ' (' + elapsed + 'ms)';
     context.font = '16px monospace';
     context.textAlign = 'left';
     context.textBaseline = 'top';
     context.fillStyle = '#ffffff';
-    context.fillText(str,0,0);
+    context.fillText(message,0,0);
   }
 }
 
