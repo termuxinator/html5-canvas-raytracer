5d38f0cfaf653973ca8e301f14c94c35af8b28f7
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-10 18:59:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -39,9 +39,9 @@ function main () {
     createSphere([1.5,2.5,0.0],0.5,createMaterial([1.0,1.0,1.0],0.1,0.3,50.0,0.8)),   // bubble
     createSphere([0.0,2.5,-2.0],0.5,createMaterial([1.0,1.0,1.0],0.1,0.8,500.0,0.6)), // mirror
     createSphere([-1.5,2.5,0.0],0.5,createMaterial([1.0,1.0,1.0],0.1,0.5,50.0,0.1)),  // metal
-    createSphere([-1.5,1.0,0.0],1.0,createMaterial([1.0,0.0,0.0],0.8,0.3,50.0,0.1)),  // orn
-    createSphere([1.5,1.0,0.0],1.0,createMaterial([0.0,1.0,0.0],0.8,0.3,50.0,0.1)),   // orn
-    createSphere([0.0,1.0,-2.0],1.0,createMaterial([0.0,0.0,1.0],0.8,0.3,50.0,0.1)),  // orn
+    createSphere([-1.5,1.0,0.0],1.0,createMaterial([1.0,0.0,0.0],0.8,0.3,50.0,0.2)),  // orn
+    createSphere([1.5,1.0,0.0],1.0,createMaterial([0.0,1.0,0.0],0.8,0.3,50.0,0.4)),   // orn
+    createSphere([0.0,1.0,-2.0],1.0,createMaterial([0.0,0.0,1.0],0.8,0.3,50.0,0.6)),  // orn
     createSphere([ 0.0,0.5,2.0],0.5,createMaterial([1.0,1.0,1.0],0.5,0.2,5.0,0.0)),   // matte
     createSphere([0.0,-5000.0,0.0],5000,createMaterial([1.0,1.0,1.0],1.0,0.5,50.0,0.4)), // world
   //createSphere([0.0,0.0,0.0],5000,createMaterial([0.4,0.6,0.8],0.0,0.0,0.0,0.0)) // skybox
