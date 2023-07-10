025a07d01b5c70686ddb93cb6fea6830ac6506ff
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-10 02:08:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -142,7 +142,13 @@ function intersectObject (objs,org,dir) {
   for (let i=0; i<objs.length; i++) {
     let o = objs[i];
     let hit = o.intersectEx(o,org,dir);
-    if (hit.t < out.t) out = hit;
+    if (hit.t < out.t) {
+      out.o = hit.o;
+      out.t = hit.t;
+      out.p = hit.p;
+      out.n = hit.n;
+      out.c = hit.c;
+    }
   }
   if (out.o != undefined) lightPoint(objs,out);
   return out;
