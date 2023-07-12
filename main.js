e69185d24e3bd14f505b282c80d2506d97ff6bf9
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-11 22:31:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -56,8 +56,8 @@ createSphere([ 0.0,0.25,4.0],0.25,createMaterial([1.0,1.0,1.0],[1.0,0.1,0.0,0.0]
   // override globe material sampler to use texture mapper
   let globe_texture = loadTexture(redraw,'./globe.png');
   objects[2].mtl.sampler = function (hit) {
-    let u = (Math.atan2(hit.p[0],-hit.p[2]) / Math.PI + 1.0) / 2;
-    let v = Math.asin(hit.p[1]) / Math.PI + 0.5;
+    let u = (Math.atan2(hit.p[0]-1.5,-(hit.p[2]-2.0)) / Math.PI + 1.0) / 2;
+    let v = Math.asin(hit.p[1]-0.5) / Math.PI + 0.5;
     u = Math.max(0, Math.ceil(u * globe_texture.width) - 1);
     v = Math.max(0, Math.ceil(v * globe_texture.height) - 1);
     let texelIndex = (v * globe_texture.width + u) * 4;
