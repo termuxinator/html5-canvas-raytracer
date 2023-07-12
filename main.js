8a6d88add2c6d03c19ea78a719b19b6319fc7556
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-11 21:19:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -58,6 +58,10 @@ createSphere([ 0.0,0.25,4.0],0.25,createMaterial([1.0,1.0,1.0],[1.0,0.1,0.0,0.0]
   objects[2].mtl.sampler = function (hit) {
     let u = Math.atan2(hit.n[0],hit.n[2]) / (Math.PI*2) + 1.0;
     let v = Math.acos(hit.n[1]) / Math.PI + 0.5;
+
+u = u*hit.n[0] + u*hit.n[1] + u*hit.n[2];
+v = v*hit.n[0] + v*hit.n[1] + v*hit.n[2];
+
     u = Math.max(0, Math.ceil(u * globe_texture.width) - 1);
     v = Math.max(0, Math.ceil(v * globe_texture.height) - 1);
     let texelIndex = (v * globe_texture.width + u) * 4;
