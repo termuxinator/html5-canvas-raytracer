fa9b41cfbf7768d90cd6b16468b33f32d3d27eb5
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-13 07:00:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let build = '513';
+let build = '514';
 
 (function() {
   let output = document.createElement('pre');
@@ -45,7 +45,7 @@ createSphere([0.0,-500.0,0.0],500,createMaterial([1.0,1.0,1.0],[0.5,0.8,0.0,0.0]
 createSphere([0.0,0.0,0.0],5000,createMaterial([0.0,0.0,0.0],[0.0,0.0,0.0,0.0],0,1.0)), // skybox
 createSphere([50.0,20.0,-100.0],4.0,createMaterial([1.0,1.0,1.0],[0.0,0.0,0.0,0.0],0,1.0)),  // earth
 createSphere([-50.0,20.0,-100.0],2.0,createMaterial([1.0,1.0,1.0],[0.0,0.0,0.0,0.0],0,1.0)),  // mars
-createSphere([0.0,0.75,4.0],0.25,createMaterial([0.5,0.5,0.5],[0.8,0.2,0.1,0.8],10,1.5)), // glass
+createSphere([0.0,0.75,4.0],0.25,createMaterial([0.5,0.5,0.5],[1.0,0.2,0.1,0.8],10,1.5)), // glass
 createSphere([ 1.5,2.5,0.0],0.5,createMaterial([1.0,1.0,1.0],[0.2,0.3,0.8,0.0],20,1.0)),  // bubble
 createSphere([0.0,2.5,-2.0],0.5,createMaterial([1.0,1.0,1.0],[0.1,0.8,0.6,0.0],500,1.0)), // mirror
 createSphere([-1.5,2.5,0.0],0.5,createMaterial([1.0,1.0,1.0],[0.8,0.2,0.1,0.0],50,1.0)),  // metal
