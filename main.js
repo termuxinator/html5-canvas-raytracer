242230421a960a86351e3d97ca6fde685be0acb2
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-12 19:30:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let build = '448';
+let build = '449';
 
 (function() {
   let output = document.createElement('pre');
@@ -28,7 +28,7 @@ function main () {
   let objects = [
 createSphere([0.0,-5000.0,0.0],5000,createMaterial([1.0,1.0,1.0],[1.0,0.5,0.0,0.0],50,1.0)), // world
 createSphere([0.0,0.0,0.0],5000,createMaterial([0.0,0.0,0.0],[0.0,0.0,0.0,0.0],0,1.0)), // skybox
-createSphere([1.5,0.5,2.0],0.5,createMaterial([1.0,1.0,1.0],[0.0,0.2,0.0,0.0],50,1.0)),  // globe
+createSphere([1.5,0.5,2.0],0.5,createMaterial([1.0,1.0,1.0],[1.0,0.2,0.0,0.0],50,1.0)),  // globe
 createSphere([0.0,0.75,4.0],0.25,createMaterial([0.5,0.5,0.5],[0.5,0.8,0.1,0.8],10,1.5)), // glass
 createSphere([ 1.5,2.5,0.0],0.5,createMaterial([1.0,1.0,1.0],[0.2,0.3,0.8,0.0],20,1.0)),  // bubble
 createSphere([0.0,2.5,-2.0],0.5,createMaterial([1.0,1.0,1.0],[0.1,0.8,0.6,0.0],500,1.0)), // mirror
@@ -56,7 +56,7 @@ createSphere([ 0.0,0.25,4.0],0.25,createMaterial([1.0,1.0,1.0],[1.0,0.1,0.0,0.0]
   // override globe material sampler to use texture mapper
   let globe_texture = loadTexture(redraw,'./globe.png');
   objects[2].mtl.sampler = function (hit) {
-    let u = Math.atan2(hit.n[0],-hit.n[2]) / Math.PI / 2 + 0.5;
+    let u = Math.atan2(hit.n[0],hit.n[2]) / Math.PI / 2 + 0.5;
     let v = Math.asin(hit.n[1]) / (Math.PI/2) / 2 + 0.5;
     u = Math.max(0, Math.ceil(u * globe_texture.width) - 1);
     v = Math.max(0, Math.ceil(v * globe_texture.height) - 1);
