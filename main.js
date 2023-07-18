b49b0283d70ea3ce2283bcf338cf2bf8512b4a5c
termuxinator <39202840+termuxinator@users.noreply.github.com>
2023-07-18 00:03:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const build = '728';
+const build = '729';
 
 (function() {
   const output = document.createElement('pre');
@@ -181,6 +181,7 @@ createSphere([0.0,1.0,-2.0],1.0,createMaterial([0.0,0.0,1.0],[0.0,0.8,0.3,0.5,0.
     const timestamp = Date.now();
     setTimeout(spanish,0,0);
     function spanish (y) {
+      let i = y * canvas.width * 4;
       for (let x=0; x<canvas.width; x++) {
         const dist = [x-projW+0.5, projH-y-0.5, projD];
         const target = [
@@ -191,16 +192,15 @@ createSphere([0.0,1.0,-2.0],1.0,createMaterial([0.0,0.0,1.0],[0.0,0.8,0.3,0.5,0.
         let ray = between3D(origin,target);
             ray = normal3D(ray);
         const rgb = intersectWorld(8,objects,origin,ray);
-        const idx = (y * canvas.width + x) * 4;
-        colorbuf.data[idx+0] = 255 * rgb[0];
-        colorbuf.data[idx+1] = 255 * rgb[1];
-        colorbuf.data[idx+2] = 255 * rgb[2];
-        colorbuf.data[idx+3] = 255;
+        colorbuf.data[i++] = 255 * rgb[0];
+        colorbuf.data[i++] = 255 * rgb[1];
+        colorbuf.data[i++] = 255 * rgb[2];
+        colorbuf.data[i++] = 255;
       }
-      context.putImageData(colorbuf,0,0,0,y,canvas.width,1);
+      //context.putImageData(colorbuf,0,0,0,y,canvas.width,1);
       if (y < canvas.height-1) setTimeout(spanish,0,y+1);
       else {
-      //context.putImageData(colorbuf,0,0,0,0,canvas.width,canvas.height);
+        context.putImageData(colorbuf,0,0,0,0,canvas.width,canvas.height);
         const elapsed = Date.now() - timestamp;
         const message = 'build #' + build + ' (' + elapsed + 'ms)';
         context.font = '16px monospace';
